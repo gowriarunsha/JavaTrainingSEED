64c1e116a9009ca10b8d02af6a3cd22407d8b6df
Gowri Arunsha <gowriarunsha@gmail.com>
2023-08-07 15:09:34 +0530

    Custom exception used

--- day3/ExceptionImp.java
+++ day3/ExceptionImp.java
@@ -1,34 +1,24 @@
 package day3;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.util.*;
 
 public class ExceptionImp {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a and b:");
         int a=sc.nextInt();
         int b=sc.nextInt();
         //arithmetic exc caught
         try{
             if(b==0)
                 throw new DivorFileException("Number cannot be divided by zero :(");
-            System.out.println(a/(double)b);
         }
         catch(DivorFileException e)
         {
             System.out.println(e.getMessage());
+            System.out.println("Enter b again:");
+            b=sc.nextInt();
         }
-        //filereader exc caught
-        try {
-            FileReader f=new FileReader("DivorFileException");
-
-        } catch (FileNotFoundException e) {
-            //throw new DivorFileException("File Not Found :(");
-        }
-
-        //throw new ArithmeticException();
-        //throw new FileNotFoundException();
-
+        System.out.println("a/b="+(a/b));
     }
 }
